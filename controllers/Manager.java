22d625e008bfeefb96b18d01cb577fd7ec6e8947
Gürkan Bıyık <gurkanbiyik00@gmail.com>
2023-04-28 21:38:00 +0300

    manager updated

--- controllers/Manager.java
+++ controllers/Manager.java
@@ -1,7 +1,29 @@
 package controllers;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Manager {
 
+    @FXML
+    Button logoutButton;
 
+    public void logout(ActionEvent event) throws IOException {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("../ui/LoginScene.fxml"));
+        Scene scene = new Scene(root);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setTitle("Hospital Management System");
+        window.setScene(scene);
+        window.show();
+    }
 	
 }
