e3765b569feb03456a912fbeebef8d6ad04bac85
Can Celenay <cancelenay@gmail.com>
2023-05-24 18:37:27 +0300

    Add enums that are needed for Patient

--- model/Patient.java
+++ model/Patient.java
@@ -1,5 +1,17 @@
 package model;
 
-public class Patient extends Person{
+import model.enums.BloodType;
+import model.enums.EmergencyState;
+import model.enums.Priority;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+public class Patient extends Person{
+    String complaint;
+    LocalDateTime appointment;
+    EmergencyState emergencyState;
+    Priority priority;
+    BloodType bloodType;
+    HashMap<Integer>
 }
---
+++ model/enums/BloodType.java
@@ -0,0 +1,17 @@
+package model.enums;
+
+/**
+ * Represents blood type of patient.
+ * P for positive, N for negative, Z for 0.
+ */
+public enum BloodType {
+    AP,
+    AN,
+    BP,
+    BZ,
+    ABP,
+    ABZ,
+    ZP,
+    ZN,
+
+}
