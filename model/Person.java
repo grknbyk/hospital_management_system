0648e69c7a6c6f10535ff655603d7d177019f3ad
Can Celenay <cancelenay@gmail.com>
2023-05-24 18:09:32 +0300

    Add Staff and Person, and other models that extend them

---
+++ model/Person.java
@@ -0,0 +1,55 @@
+package model;
+
+abstract public class Person {
+	private int id;
+	private String name;
+	private String surname;
+	private Gender gender;
+	private int age;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	enum Gender{
+		MALE,
+		FEMALE
+	}
+}
