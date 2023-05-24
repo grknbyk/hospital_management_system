10dd2fe3017ca975ccd3c9f3326cf88b0130da94
Can Celenay <cancelenay@gmail.com>
2023-05-24 18:38:19 +0300

    Add MedicineType

---
+++ model/enums/MedicineType.java
@@ -0,0 +1,15 @@
+package model.enums;
+
+public enum MedicineType {
+    //https://www.gosh.nhs.uk/conditions-and-treatments/medicines-information/types-medicines/
+    LIQUID,
+    TABLET,
+    CAPSULES,
+    TOPICAL,
+    SUPPOSITORIES,
+    DROPS,
+    INHALERS,
+    INJECTIONS,
+    PATCHES,
+    BUCCAL
+}
