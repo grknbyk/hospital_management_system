368875b9245205923d0e0a93fbf91a6456344f96
Can Celenay <cancelenay@gmail.com>
2023-05-24 19:51:47 +0300

    Add Medicine

---
+++ model/Medicine.java
@@ -0,0 +1,14 @@
+package model;
+
+import model.enums.MedicineType;
+
+public class Medicine {
+    MedicineType type;
+    String name;
+
+    @Override
+    public int hashCode()
+    {
+        return name.hashCode();
+    }
+}
