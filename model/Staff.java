a49887be6d5abd3038c447bf8f58db8c56214cb9
Gürkan Bıyık <gurkanbiyik00@gmail.com>
2023-05-25 18:58:41 +0300

    db update, queryStaff function in datasource, created contact model and added as field in staff

--- model/Staff.java
+++ model/Staff.java
@@ -1,8 +1,28 @@
 package model;
 
+import model.enums.Status;
+
 public abstract class Staff extends Person {
 	private String username;
 	private String password;
+	private Status status;	
+	private Contact contact;
+	 
+	public Contact getContact() {
+		return contact;
+	}
+
+	public void setContact(Contact contact) {
+		this.contact = contact;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
 
 	public String getUsername() {
 		return username;
---
+++ model/enums/Status.java
@@ -0,0 +1,5 @@
+package model.enums;
+
+public enum Status {
+    MANAGER, DOCTOR, NURSE, PHARMACIST, RECEPTIONIST
+}
