9427d4875de9589235c2c25b78a039c54a095e97
Abdulkadir Öksüz <abdulkadir.oksuz@outlook.com>
2023-05-25 19:47:45 +0300

    profile dialog added

--- ui/controllers/ProfileController.java
+++ ui/controllers/ProfileController.java
@@ -1,2 +1,41 @@
-package ui.controllers;public class ProfileController {
+package ui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class ProfileController {
+
+    @FXML
+    private Label AgeField;
+
+    @FXML
+    private Label GenderField;
+
+    @FXML
+    private Label SurnameField;
+
+    @FXML
+    private Label addressField;
+
+    @FXML
+    private Label emailField;
+
+    @FXML
+    private ImageView imageField;
+
+    @FXML
+    private Label nameField;
+
+    @FXML
+    private Label phoneField;
+
+    @FXML
+    private Label statusField;
+
+    public void initialize(){
+        imageField.setImage(new Image("ui/imgs/default_person.png"));
+    }
+
 }
