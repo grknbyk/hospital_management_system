149628608acbba3251c927079454e0e1ea10b7de
Abdulkadir Öksüz <abdulkadir.oksuz@outlook.com>
2023-05-25 20:50:31 +0300

    Manager panel updated

--- ui/controllers/ManagerController.java
+++ ui/controllers/ManagerController.java
@@ -1,8 +1,12 @@
 package ui.controllers;
 
+import database.Datasource;
 import java.io.IOException;
 import java.util.Optional;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,6 +18,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import model.Staff;
 
 public class ManagerController {
 
@@ -22,6 +27,17 @@ public class ManagerController {
 
     @FXML
     private MenuItem logoutMenuItem;
+
+    @FXML
+    public void listStaffs() {
+        Task<ObservableList<Staff>> task = new GetAllStaffsTask();
+        //artistTable.itemsProperty().bind(task.valueProperty());
+
+        new Thread(task).start();
+    }
+
+    public void initialize() {
+    }
 
     public void logout(ActionEvent event) throws IOException {
         
@@ -52,13 +68,13 @@ public class ManagerController {
         dialog.getDialogPane().getButtonTypes().add(ButtonType.CLOSE);
 
         Optional<ButtonType> result = dialog.showAndWait();
-//        if(result.isPresent() && result.get() == ButtonType.CLOSE) {
-//            ProfileController contactController = fxmlLoader.getController();
-//            Contact newContact = contactController.getNewContact();
-//            data.addContact(newContact);
-//            data.saveContacts();
-//        }
     }
 
 	
+}
+class GetAllStaffsTask extends Task {
+    @Override
+    public ObservableList<Staff> call() throws Exception {
+        return FXCollections.observableArrayList(Datasource.getInstance().queryStaff());
+    }
 }
