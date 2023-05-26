0acd9dc1eb7dcd15aaa312eaf6f546b09b6758fb
Abdulkadir Öksüz <abdulkadir.oksuz@outlook.com>
2023-05-27 02:20:02 +0300

    Doctor table Filled
    Acil olanlar sırada aşağı kaymamalı ilerde yapılacak

--- model/Patient.java
+++ model/Patient.java
@@ -1,5 +1,8 @@
 package model;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import model.enums.BloodType;
 import model.enums.EmergencyState;
 import model.enums.Priority;
@@ -7,44 +10,61 @@ import model.enums.Priority;
 import java.time.LocalDateTime;
 
 public class Patient extends Person{
-    String complaint;
-    LocalDateTime appointment;
-    EmergencyState emergencyState;
-    Priority priority;
-    BloodType bloodType;
-
+    SimpleStringProperty complaint = new SimpleStringProperty("");
+    ObjectProperty<LocalDateTime> appointment = new SimpleObjectProperty<>();
+    ObjectProperty<EmergencyState> emergencyState = new SimpleObjectProperty<>();
+    ObjectProperty<Priority> priority = new SimpleObjectProperty<>();
+    ObjectProperty<BloodType> bloodType = new SimpleObjectProperty<>();
+    SimpleStringProperty hour = new SimpleStringProperty("");
     public Patient(){
 
     }
 
+    public String getHour() {
+        return hour.get();
+    }
+
+    public SimpleStringProperty hourProperty() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour.set(hour);
+    }
+
     public String getComplaint() {
-        return complaint;
+        return complaint.get();
     }
     public void setComplaint(String complaint) {
-        this.complaint = complaint;
+        this.complaint.set(complaint);
     }
     public LocalDateTime getAppointment() {
-        return appointment;
+        return appointment.get();
     }
     public void setAppointment(LocalDateTime appointment) {
-        this.appointment = appointment;
+        this.appointment.set(appointment);
+        try {
+            this.hour.set(appointment.getHour()+":"+appointment.getMinute());
+        }catch (Exception e){
+
+        }
     }
     public EmergencyState getEmergencyState() {
-        return emergencyState;
+        return emergencyState.get();
     }
     public void setEmergencyState(EmergencyState emergencyState) {
-        this.emergencyState = emergencyState;
+        this.emergencyState.set(emergencyState);
     }
     public Priority getPriority() {
-        return priority;
+        return priority.get();
     }
     public void setPriority(Priority priority) {
-        this.priority = priority;
+        this.priority.set(priority);
     }
     public BloodType getBloodType() {
-        return bloodType;
+        return bloodType.get();
     }
     public void setBloodType(BloodType bloodType) {
-        this.bloodType = bloodType;
+        this.bloodType.set(bloodType);
     }
 }
