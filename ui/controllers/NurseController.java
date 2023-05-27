0db57cef820e7ed3bede1c714ad022e1501d7173
Can Celenay <cancelenay@gmail.com>
2023-05-27 09:24:02 +0300

    Remake Nurse

--- ui/controllers/NurseController.java
+++ ui/controllers/NurseController.java
@@ -1,22 +1,115 @@
 package ui.controllers;
 
+import database.Datasource;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import model.Patient;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class NurseController {
-    String userName;
-    
+    String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @FXML
+    private BorderPane nursePanel;
+
     @FXML
     private MenuItem logoutMenuItem;
 
-    public void showProfileDialog(ActionEvent actionEvent) {
+    @FXML
+    private TableView<Patient> patientTableView;
+    private ObservableList<Patient> patients;
+
+    @FXML
+    private MenuButton options;
+
+
+    public void initialize() {
+
+        Image menuImg = new Image("ui/imgs/default_person.png");
+        ImageView imageView = new ImageView(menuImg);
+        imageView.setFitHeight(18);
+        imageView.setFitWidth(18);
+        options.setGraphic(imageView);
+    }
+
+    public void loadPatients(){
+        //fill the table
+        int staffId = Datasource.getInstance().queryStaffId(username);
+        patients = FXCollections.observableArrayList(Datasource.getInstance().queyPatients(staffId));
+        patientTableView.setItems(patients);
+    }
+
+    public void showProfileDialog(){
+
+        Dialog<ButtonType> dialog2 = new Dialog<ButtonType>();
+        dialog2.initOwner(nursePanel.getScene().getWindow());
+        dialog2.setTitle("Profile");
+        FXMLLoader fxmlLoader2 = new FXMLLoader();
+        fxmlLoader2.setLocation(getClass().getResource("../scene/profile/ProfileDialog.fxml"));
+        try {
+            dialog2.getDialogPane().setContent(fxmlLoader2.load());
+            ProfileController profileController = fxmlLoader2.getController();
+            profileController.updateFields(username);
+        } catch(IOException e) {
+            System.out.println("Couldn't load the dialog");
+            e.printStackTrace();
+            return;
+        }
+
+        ButtonType editButton = new ButtonType("Edit");
+        ButtonType closeButton = new ButtonType("Close");
+
+        dialog2.getDialogPane().getButtonTypes().addAll(editButton, closeButton);
+
+        Optional<ButtonType> result = dialog2.showAndWait();
+        if(result.isPresent() && result.get() == editButton) {
+            Dialog<ButtonType> dialog = new Dialog<ButtonType>();
+            dialog.initOwner(nursePanel.getScene().getWindow());
+            dialog.setTitle("Edit Profile");
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("../scene/profile/EditProfile.fxml"));
+
+            try {
+                dialog.getDialogPane().setContent(fxmlLoader.load());
+                ProfileController profileController = fxmlLoader.getController();
+                profileController.updateEdit(username);
+            }catch (IOException e){
+                System.out.println("Couldn't load the dialog");
+                e.printStackTrace();
+                return;
+            }
+
+            dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+            dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+            Optional<ButtonType> result2 = dialog.showAndWait();
+            if(result2.isPresent() && result2.get() == ButtonType.OK) {
+
+                ProfileController profileController = fxmlLoader.getController();
+                profileController.editFields(username);
+            }
+
+        }
     }
 
     public void logout(ActionEvent event) throws IOException {
