dc3b9f9aee1af7b39dda9c96aef56853b8d9dab1
Can Celenay <cancelenay@gmail.com>
2023-05-27 20:42:36 +0300

    Fix receptionist logout

--- ui/controllers/ReceptionistController.java
+++ ui/controllers/ReceptionistController.java
@@ -1,12 +1,16 @@
 package ui.controllers;
 
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
@@ -20,12 +24,10 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
+import model.Patient;
 
 public class ReceptionistController {
     String username;
-
-    @FXML
-    private BorderPane receptionistPanel;
 
     public String getUsername() {
         return username;
@@ -35,12 +37,30 @@ public class ReceptionistController {
         this.username = username;
     }
 
+    @FXML
+    private BorderPane receptionistPanel;
+
+    @FXML
+    private MenuItem logoutMenuItem;
+
+    @FXML
+    private TableView<Patient> patientTableView;
+    private ObservableList<Patient> patients;
+
+    @FXML
+    private MenuButton options;
+
     public void showProfileDialog() {
         new ProfileViewBuilder(username, receptionistPanel).showProfileView();
     }
 
-    public void logout() {
-        System.out.println("Logging out");
+    public void logout(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../scene/LoginScene.fxml"));
+        Scene scene = new Scene(root);
+        Stage window = (Stage) logoutMenuItem.getParentPopup().getOwnerNode().getScene().getWindow();
+        window.setTitle("Hospital Management System");
+        window.setScene(scene);
+        window.show();
     }
 
     public void showAboutDialog() {
