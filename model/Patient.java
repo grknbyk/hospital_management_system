e505fb747af9b00edade53c9ca5ffede50e6267a
Can Celenay <cancelenay@gmail.com>
2023-05-28 16:51:12 +0300

    Add receiptId and doctorId to Patient

--- model/Patient.java
+++ model/Patient.java
@@ -15,6 +15,8 @@ public class Patient extends Person{
     ObjectProperty<EmergencyState> emergencyState = new SimpleObjectProperty<>();
     ObjectProperty<Priority> priority = new SimpleObjectProperty<>();
     ObjectProperty<BloodType> bloodType = new SimpleObjectProperty<>();
+    ObjectProperty<Integer> doctorId = new SimpleObjectProperty<>();
+    ObjectProperty<Integer> receiptId = new SimpleObjectProperty<>();
     SimpleStringProperty hour = new SimpleStringProperty("");
     public Patient(){
 
@@ -66,5 +68,21 @@ public class Patient extends Person{
     }
     public void setBloodType(BloodType bloodType) {
         this.bloodType.set(bloodType);
+    }
+
+    public ObjectProperty<Integer> getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(ObjectProperty<Integer> doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public ObjectProperty<Integer> getReceiptId() {
+        return receiptId;
+    }
+
+    public void setReceiptId(ObjectProperty<Integer> receiptId) {
+        this.receiptId = receiptId;
     }
 }
