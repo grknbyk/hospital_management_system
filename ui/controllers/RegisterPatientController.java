1c77dc5e52806c92cd89da8bf3bb32b47e258b53
Abdulkadir Öksüz <abdulkadir.oksuz@outlook.com>
2023-05-29 01:28:02 +0300

    Receptionist register functionalized
    patient update yapılacak

--- ui/controllers/RegisterPatientController.java
+++ ui/controllers/RegisterPatientController.java
@@ -1,19 +1,36 @@
 package ui.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import model.Patient;
+
+import java.time.LocalDate;
 
 public class RegisterPatientController {
 
     @FXML
     private TextArea complaintTextArea;
 
+
     @FXML
     private TextArea emergencyTextArea;
 
     @FXML
     private TextArea priorityTextArea;
+
+    @FXML
+    private ChoiceBox<String> policlinicBox;
+    private String[] policlinicks = {"Cardiology", "Neurology", "Orthopedic Surgery", "Oncology", "Pediatrics", "Psychiatry", "Dermatology", "Ophthalmology", "Anesthesiology", "Cardiology"};
+
+    @FXML
+    private ChoiceBox<String> serviceBox;
+    private String[] serviceAreas = {"Intensive Care Unit","Emergency Department", "Operating Room", "Medical-Surgical Ward"};
+
+    @FXML
+    private DatePicker datePicker;
+
 
     public void updateFields(Patient patient) {
         complaintTextArea.setText(patient.getComplaint());
@@ -24,5 +41,8 @@ public class RegisterPatientController {
 
         priorityTextArea.setText(patient.getPriority().toString());
         priorityTextArea.setEditable(false);
+
+        serviceBox.getItems().addAll(serviceAreas);
+        policlinicBox.getItems().addAll(policlinicks);
     }
 }
