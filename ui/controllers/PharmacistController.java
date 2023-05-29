a8f7d95c6e3277bec804da49facc780328433b9e
Can Celenay <cancelenay@gmail.com>
2023-05-29 17:10:45 +0300

    New menu items

--- ui/controllers/PharmacistController.java
+++ ui/controllers/PharmacistController.java
@@ -5,11 +5,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.Dialog;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -32,19 +30,14 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
 import model.Medicine;
 import model.MedicineSupply;
 import model.Receipt;
-import model.enums.MedicineType;
-import utils.Pair;
 
 
 public class PharmacistController {
@@ -115,6 +108,9 @@ public class PharmacistController {
         window.show();
     }
 
+    public void dispenseMedicine() {
+
+    }
 
     public void showHelpDialog() {
         // Create a new stage for the dialog
@@ -172,7 +168,6 @@ public class PharmacistController {
         dialogStage.setScene(dialogScene);
         dialogStage.show();
     }
-
 
 
     public void showAboutDialog() {
