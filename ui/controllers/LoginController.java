fbe3b45cb0494f4c4b9f84589eb2669361d5fb94
Abdulkadir Öksüz <abdulkadir.oksuz@outlook.com>
2023-05-29 18:00:35 +0300

    Add medicine

--- ui/controllers/LoginController.java
+++ ui/controllers/LoginController.java
@@ -50,8 +50,8 @@ public class LoginController {
         String username = usernameTextField.getText();
         String password = passwordTextField.getText();
 
-        username = "admin";
-        password = "admin";
+        //username = "admin";
+        //password = "admin";
 
 		String status = Datasource.getInstance().queryLogin(username, password);
 
