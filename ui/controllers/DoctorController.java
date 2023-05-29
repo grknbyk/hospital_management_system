3740b527147e61e9dcdd9db882ca5bf580effb40
Abdulkadir Öksüz <abdulkadir.oksuz@outlook.com>
2023-05-29 13:02:44 +0300

    Help & About dialogs completed
    mailinizi eklersiniz

--- ui/controllers/DoctorController.java
+++ ui/controllers/DoctorController.java
@@ -76,11 +76,16 @@ public class DoctorController {
         patientTableView.setItems(patients);
     }
 
-    public void showAboutDialog() {
+
+    public void showHelpDialog() {
         // Create a new stage for the dialog
         Stage dialogStage = new Stage();
         dialogStage.initModality(Modality.APPLICATION_MODAL);
         dialogStage.setTitle("Help");
+
+        // Create a text area for additional instructions or information
+        Label additionalInfoLabel = new Label();
+        additionalInfoLabel.setText("Contact us via our contact addresses for error reporting. \nCheck out our tutorial content on our website.");
 
         // Create labels and fields for email, phone, and website
         Label emailLabel = new Label("Email:");
@@ -105,12 +110,8 @@ public class DoctorController {
             }
         });
 
-        // Create a text area for additional instructions or information
-        Label additionalInfoLabel = new Label();
-        additionalInfoLabel.setText("This project is designed for OOP lecture in Dokuz Eylul University");
-
         // Create a button to close the dialog
-        javafx.scene.control.Button closeButton = new Button("Close");
+        Button closeButton = new Button("Close");
         closeButton.setOnAction(event -> dialogStage.close());
 
         // Create a grid pane to hold the dialog content
@@ -119,19 +120,98 @@ public class DoctorController {
         gridPane.setVgap(10);
         gridPane.setPadding(new Insets(10));
         gridPane.setAlignment(Pos.CENTER);
-        gridPane.add(emailLabel, 0, 0);
-        gridPane.add(emailField, 1, 0);
-        gridPane.add(phoneLabel, 0, 1);
-        gridPane.add(phoneField, 1, 1);
-        gridPane.add(websiteLink, 0, 2, 2, 1);
-        gridPane.add(additionalInfoLabel, 0, 3, 2, 1);
-        gridPane.add(closeButton, 0, 4, 2, 1);
+        gridPane.add(additionalInfoLabel, 0, 0, 2, 1);
+        gridPane.add(emailLabel, 0, 1);
+        gridPane.add(emailField, 1, 1);
+        gridPane.add(phoneLabel, 0, 2);
+        gridPane.add(phoneField, 1, 2);
+        gridPane.add(websiteLink, 0, 3, 2, 1);
+        gridPane.add(closeButton, 5, 4, 2, 1);
 
         // Set the scene for the dialog and show it
         Scene dialogScene = new Scene(gridPane);
         dialogStage.setScene(dialogScene);
         dialogStage.show();
     }
+
+
+    public void showAboutDialog() {
+        // Create a new stage for the dialog
+        Stage dialogStage = new Stage();
+        dialogStage.initModality(Modality.APPLICATION_MODAL);
+        dialogStage.setTitle("About");
+
+        // Create a text area for additional instructions or information
+        Label additionalInfoLabel = new Label();
+        additionalInfoLabel.setText("Who are we?\nWe are computer engineering students at Dokuz Eylül University. \nWe developed this project for our school's OOP class. You can contact us via the following e-mail addresses.");
+
+        // Create labels and fields for email, phone, and website
+        Label emailLabel = new Label("Abdulkadir Öksüz:");
+        Hyperlink emailField = new Hyperlink("abdulkadir.oksuz@outlook.com");
+        emailField.setPrefWidth(200);
+        emailField.setOnMouseClicked(event -> {
+            copyToClipboard(emailField.getText());
+            showAlert(Alert.AlertType.INFORMATION, "Copied", "Email address copied to clipboard.");
+        });
+
+        Label emailLabel2 = new Label("Can Çelenay:");
+        Hyperlink emailField2 = new Hyperlink("email");
+        emailField2.setPrefWidth(200);
+        emailField2.setOnMouseClicked(event -> {
+            copyToClipboard(emailField2.getText());
+            showAlert(Alert.AlertType.INFORMATION, "Copied", "Email address copied to clipboard.");
+        });
+
+        Label emailLabel3 = new Label("Gürkan Bıyık:");
+        Hyperlink emailField3 = new Hyperlink("email");
+        emailField3.setPrefWidth(200);
+        emailField3.setOnMouseClicked(event -> {
+            copyToClipboard(emailField3.getText());
+            showAlert(Alert.AlertType.INFORMATION, "Copied", "Email address copied to clipboard.");
+        });
+
+        Label phoneLabel = new Label("Phone:");
+        Label phoneField = new Label("+1 123-456-7890");
+
+        Hyperlink websiteLink = new Hyperlink("Website: https://ceng.deu.edu.tr");
+        websiteLink.setOnAction(event -> {
+            try {
+                Desktop.getDesktop().browse(new URI("https://ceng.deu.edu.tr"));
+            } catch (IOException | URISyntaxException e) {
+                // Handle any errors that occur while trying to open the link
+                e.printStackTrace();
+                showAlert(Alert.AlertType.ERROR, "Error", "Failed to open website.");
+            }
+        });
+
+        // Create a button to close the dialog
+        Button closeButton = new Button("Close");
+        closeButton.setOnAction(event -> dialogStage.close());
+
+        // Create a grid pane to hold the dialog content
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(10));
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.add(additionalInfoLabel, 0, 0, 2, 1);
+        gridPane.add(emailLabel, 0, 1);
+        gridPane.add(emailField, 1, 1);
+        gridPane.add(emailLabel2, 0, 2);
+        gridPane.add(emailField2, 1, 2);
+        gridPane.add(emailLabel3, 0, 3);
+        gridPane.add(emailField3, 1, 3);
+        gridPane.add(phoneLabel, 0, 4);
+        gridPane.add(phoneField, 1, 4);
+        gridPane.add(websiteLink, 0, 5, 2, 1);
+        gridPane.add(closeButton, 5, 6, 2, 1);
+
+        // Set the scene for the dialog and show it
+        Scene dialogScene = new Scene(gridPane);
+        dialogStage.setScene(dialogScene);
+        dialogStage.show();
+    }
+
 
     private void copyToClipboard(String text) {
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
